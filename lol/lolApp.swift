717af5bc3514348c3c057fa282a18b4d9c9bfe8a
a.m.student <ninjaalec12@gmail.com>
2021-01-20 10:22:15 -0600

    Initial Commit

---
+++ lol/lolApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolApp.swift
+//  lol
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct lolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
